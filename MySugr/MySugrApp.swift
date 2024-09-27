ee50862fe1a899d5f2aacad7b749d15a8852591b
Tomáš Bobko <kalafun@gmail.com>
2024-09-27 13:01:16 +0200

    Initial Commit

---
+++ MySugr/MySugrApp.swift
@@ -0,0 +1,17 @@
+//
+//  MySugrApp.swift
+//  MySugr
+//
+//  Created by Tomas Bobko on 27.09.24.
+//
+
+import SwiftUI
+
+@main
+struct MySugrApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ MySugrTests/MySugrTests.swift
@@ -0,0 +1,17 @@
+//
+//  MySugrTests.swift
+//  MySugrTests
+//
+//  Created by Tomas Bobko on 27.09.24.
+//
+
+import Testing
+@testable import MySugr
+
+struct MySugrTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
