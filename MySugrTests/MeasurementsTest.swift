28a1cac56996029562d59e5f937c35105607e7e0
Tomáš Bobko <kalafun@gmail.com>
2024-09-29 14:38:25 +0200

    - Add measurementsTest file for testing MeasurementsViewModel - Add test for measurementsCount - Add test for empty state - Add test for creating mocked measurements - Add test for string from measurement

---
+++ MySugrTests/MeasurementsTest.swift
@@ -0,0 +1,62 @@
+//
+//  MeasurementsTest.swift
+//  MySugrTests
+//
+//  Created by Tomas Bobko on 29.09.24.
+//
+
+import XCTest
+import SwiftUI
+@testable import MySugr
+
+class MeasurementsTest: XCTestCase {
+
+    var viewModel: MeasurementsView.ViewModel!
+
+    override func setUp() async throws {
+
+        await MainActor.run {
+            viewModel = MeasurementsView.ViewModel(
+                measurements: [
+                    Measurement(value: 5),
+                    Measurement(value: 10),
+                    Measurement(value: 15)
+                ]
+            )
+        }
+    }
+
+    override func tearDown() {
+        viewModel = nil
+    }
+
+    @MainActor
+    func testMeasurementsCount() {
+        let measurementsView = MeasurementsView(viewModel: viewModel)
+
+        XCTAssertEqual(
+            measurementsView.viewModel.measurements.count, 3, "The view model should have three measurements."
+        )
+    }
+
+    @MainActor
+    func testEmptyState() {
+        viewModel.measurements = []
+
+        XCTAssertTrue(viewModel.measurements.isEmpty, "The measurements array should be empty.")
+    }
+
+    @MainActor
+    func testCreatingMockedMeasurements() {
+        let measurements = Measurement.mocked(count: 20)
+
+        XCTAssertTrue(measurements.count == 20)
+    }
+
+    @MainActor
+    func testStringFromMeasurement() {
+        let measurement = Measurement(value: 3.3)
+
+        XCTAssertEqual(viewModel.stringFromMeasurement(measurement), "3,3")
+    }
+}
